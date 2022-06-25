13b53c0bc1e217184f12e5f87b147f0a3a18cb22
Ningaro <23503775+Ningaro@users.noreply.github.com>
2022-06-26 01:40:00 +0300

    ✨ Add MTUCI map prototype

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,44 +1,66 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { type Dispatch, type SetStateAction, useState } from 'react'
+import { Box, Stack, Text } from '@chakra-ui/react'
+import l from 'leaflet'
 import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet'
 
 // Styles
 import 'leaflet/dist/leaflet.css'
+import './leafletStyles.css'
 
 export default function LeafletMap() {
+  const [center, setCenter] = useState<{ x: number; y: number }>({
+    x: 533,
+    y: -253,
+  })
+  const [zoom, setZoom] = useState<number>(1)
   return (
-    <Flex
+    <Stack
       boxSize="100%"
       align="center"
       justify="center"
       px={{ base: 2, lg: 48 }}
       py={{ base: 40, lg: 32 }}
     >
+      <Text>
+        Center x: {center.x}, y: {center.y}
+      </Text>
+      <Text>Zoom {zoom}</Text>
       <Box boxSize="100%">
         <MapContainer
-          center={[4.5, 6]}
-          zoom={9}
+          crs={l.CRS.Simple}
+          center={[center.y, center.x]}
+          zoom={zoom}
           style={{ width: '100%', height: '100%' }}
           attributionControl={false}
         >
-          <MapInfo />
+          <MapInfo setZoom={setZoom} setCenter={setCenter} />
           <TileLayer
             // url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-            url="https://map.aviapark.com/tiles/1@2x/{z}/{x}/{y}.png"
-            maxZoom={11}
-            minZoom={8}
+            url={`${process.env.PUBLIC_URL}/imgs/{z}/{x}-{y}.png`}
+            // url="https://map.aviapark.com/tiles/1@2x/{z}/{x}/{y}.png"
+            maxZoom={zoom + 1}
+            minZoom={zoom - 1}
           />
         </MapContainer>
       </Box>
-    </Flex>
+    </Stack>
   )
 }
 
-function MapInfo() {
+function MapInfo({ setCenter, setZoom }: MapInfoProps) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const map = useMapEvents({
     move: () => {
-      // console.warn('location found:', map.getZoom())
+      // console.warn('location found:', map.getCenter())
+      setZoom(map.getZoom())
+      setCenter({ x: map.getCenter().lng, y: map.getCenter().lat })
     },
   })
   return null
+}
+
+// Types
+interface MapInfoProps {
+  setZoom: Dispatch<number>
+  setCenter: Dispatch<SetStateAction<{ x: number; y: number }>>
 }
