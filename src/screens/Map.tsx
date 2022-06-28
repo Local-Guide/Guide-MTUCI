34889ff7fb9de1d85cfccc4086bef1d0d2f10893
Mikhail Iugai <mishayugai53@gmail.com>
2022-06-29 02:05:08 +0300

    :sparkles: Turn map into fullscreen, change ZoomControl btns and set maxBounds

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -1,7 +1,7 @@
-import { type Dispatch, type SetStateAction, useState } from 'react'
+import { type Dispatch, type SetStateAction, useState, useEffect } from 'react'
 import { Box, Stack, Text } from '@chakra-ui/react'
 import l from 'leaflet'
-import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet'
+import { MapContainer, TileLayer, useMapEvents, ZoomControl } from 'react-leaflet'
 
 // Styles
 import 'leaflet/dist/leaflet.css'
@@ -9,41 +9,31 @@ import './leafletStyles.css'
 
 export default function LeafletMap() {
   const [center, setCenter] = useState<{ x: number; y: number }>({
-    x: 533,
-    y: -253,
+    x: -253,
+    y: 533,
   })
+  const bounds: [[number, number]] = [[center.x, center.y]]
   const [zoom, setZoom] = useState<number>(1)
   return (
-    <Stack
-      boxSize="100%"
-      align="center"
-      justify="center"
-      px={{ base: 2, lg: 48 }}
-      py={{ base: 40, lg: 32 }}
+    <MapContainer
+      maxBounds={bounds}
+      crs={l.CRS.Simple}
+      center={[center.y, center.x]}
+      zoom={zoom}
+      className='map'
+      attributionControl={false}
+      zoomControl={false}
     >
-      <Text>
-        Center x: {center.x}, y: {center.y}
-      </Text>
-      <Text>Zoom {zoom}</Text>
-      <Box boxSize="100%">
-        <MapContainer
-          crs={l.CRS.Simple}
-          center={[center.y, center.x]}
-          zoom={zoom}
-          style={{ width: '100%', height: '100%' }}
-          attributionControl={false}
-        >
-          <MapInfo setZoom={setZoom} setCenter={setCenter} />
-          <TileLayer
-            // url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-            url={`${process.env.PUBLIC_URL}/imgs/{z}/{x}-{y}.png`}
-            // url="https://map.aviapark.com/tiles/1@2x/{z}/{x}/{y}.png"
-            maxZoom={zoom + 1}
-            minZoom={zoom - 1}
-          />
-        </MapContainer>
-      </Box>
-    </Stack>
+      <MapInfo setZoom={setZoom} setCenter={setCenter} />
+      <TileLayer
+        // url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        url={`${process.env.PUBLIC_URL}/imgs/{z}/{x}-{y}.png`}
+        // url="https://map.aviapark.com/tiles/1@2x/{z}/{x}/{y}.png"
+        maxZoom={zoom + 1}
+        minZoom={zoom - 1}
+      />
+      <ZoomControl position='topright' zoomInText='🥪' zoomOutText='🎸'/>
+    </MapContainer>
   )
 }
 
@@ -51,9 +41,9 @@ function MapInfo({ setCenter, setZoom }: MapInfoProps) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const map = useMapEvents({
     move: () => {
-      // console.warn('location found:', map.getCenter())
+      console.warn('location found:', map.getCenter())
       setZoom(map.getZoom())
-      setCenter({ x: map.getCenter().lng, y: map.getCenter().lat })
+      setCenter({ x: map.getCenter().lng, y: map.getCenter().lat})
     },
   })
   return null
