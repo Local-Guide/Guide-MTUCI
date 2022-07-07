749868ba57803c997687dce550384b49cdfbf421
dashasch708 <dashacomely148@gmail.com>
2022-07-07 14:49:59 +0300

    autocomplete failed

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -9,10 +9,56 @@ import {
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
 import { SearchIcon } from '@chakra-ui/icons'
+import { useState } from 'react'
 
 import '@fontsource/inter'
 
 export default function Search() {
+  const [loopOpacity, setLoopOpacity] = useState<string>('0.3')
+
+  type Item = {
+    id: number
+    name: string
+  }
+
+  const items: Item[] = [
+    { id: 301, name: '301' },
+    { id: 302, name: '302' },
+  ]
+
+  // const handleOnSearch = (string: any, results: any) => {
+  //   // onSearch will have as the first callback parameter
+  //   // the string searched and for the second the results.
+  //   console.log(string, results)
+  // }
+
+  // const handleOnHover = (result: any) => {
+  //   // the item hovered
+  //   console.log(result)
+  // }
+
+  // const handleOnSelect = (item: any) => {
+  //   // the item selected
+  //   console.log(item)
+  // }
+
+  // const handleOnFocus = () => {
+  //   console.log('Focused')
+  // }
+
+  const formatResult = (item: any) => {
+    return (
+      <>
+        <span style={{ display: 'block', textAlign: 'left' }}>
+          id: {item.id}
+        </span>
+        <span style={{ display: 'block', textAlign: 'left' }}>
+          name: {item.name}
+        </span>
+      </>
+    )
+  }
+
   return (
     <Center>
       {' '}
@@ -36,11 +82,21 @@ export default function Search() {
               fontFamily: 'Inter',
               fontWeight: '700',
             }}
+            onInput={() => setLoopOpacity('1')}
+          />
+          <ReactSearchAutocomplete<Item>
+            items={items}
+            // onSearch={handleOnSearch}
+            // onHover={handleOnHover}
+            // onSelect={handleOnSelect}
+            // onFocus={handleOnFocus}
+            autoFocus
+            formatResult={formatResult}
           />
           <InputRightElement h="-webkit-fill-available">
             <SearchIcon
               color="white"
-              opacity="0.3"
+              opacity={loopOpacity}
               mr={{ base: '2', lg: '4' }}
             />
           </InputRightElement>
