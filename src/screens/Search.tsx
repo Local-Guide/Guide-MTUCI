352428d1994821428ab9b1fcaf3a3eee47bebceb
dashasch708 <dashacomely148@gmail.com>
2022-07-08 23:57:44 +0300

    ✨autocomplete
    +removed package <ReactSearchAutocomplete>

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -6,7 +6,7 @@ import {
   Input,
 } from '@chakra-ui/react'
 
-import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+import './autocomplete.css'
 
 import { SearchIcon } from '@chakra-ui/icons'
 import { useState } from 'react'
@@ -14,6 +14,7 @@ import { useState } from 'react'
 import '@fontsource/inter'
 
 export default function Search() {
+  const [value, setValue] = useState<string>('')
   const [loopOpacity, setLoopOpacity] = useState<string>('0.3')
 
   type Item = {
@@ -22,85 +23,70 @@ export default function Search() {
   }
 
   const items: Item[] = [
+    { id: 300, name: '300' },
     { id: 301, name: '301' },
     { id: 302, name: '302' },
+    { id: 303, name: '303' },
+    { id: 304, name: '304' },
+    { id: 305, name: '305' },
+    { id: 306, name: '306' },
+    { id: 307, name: '307' },
+    { id: 308, name: '308' },
+    { id: 309, name: '309' },
+    { id: 310, name: '310' },
+    { id: 311, name: '311' },
+    { id: 1, name: 'Туалет' },
   ]
 
-  // const handleOnSearch = (string: any, results: any) => {
-  //   // onSearch will have as the first callback parameter
-  //   // the string searched and for the second the results.
-  //   console.log(string, results)
-  // }
-
-  // const handleOnHover = (result: any) => {
-  //   // the item hovered
-  //   console.log(result)
-  // }
-
-  // const handleOnSelect = (item: any) => {
-  //   // the item selected
-  //   console.log(item)
-  // }
-
-  // const handleOnFocus = () => {
-  //   console.log('Focused')
-  // }
-
-  const formatResult = (item: any) => {
-    return (
-      <>
-        <span style={{ display: 'block', textAlign: 'left' }}>
-          id: {item.id}
-        </span>
-        <span style={{ display: 'block', textAlign: 'left' }}>
-          name: {item.name}
-        </span>
-      </>
-    )
-  }
+  const fitlerCabinets = items.filter((item) =>
+    item.name.toLowerCase().startsWith(value.toLowerCase())
+  )
 
   return (
     <Center>
       {' '}
-      <Flex w="60vw">
-        <InputGroup mt="10" position="relative" zIndex="999">
-          <Input
-            h={{ base: '2em', lg: '2.5rem' }}
-            bg="gray.700"
-            w="60vw"
-            opacity="0.9"
-            borderRadius="17px"
-            color="white"
-            fontWeight="700"
-            filter="drop-shadow(9px 7px 20px rgba(0, 0, 0, 0.4))"
-            border="none"
-            fontSize={{ base: 'revert', lg: 'md' }}
-            placeholder="Поиск..."
-            _placeholder={{
-              color: 'white',
-              opacity: 0.3,
-              fontFamily: 'Inter',
-              fontWeight: '700',
-            }}
-            onInput={() => setLoopOpacity('1')}
-          />
-          <ReactSearchAutocomplete<Item>
-            items={items}
-            // onSearch={handleOnSearch}
-            // onHover={handleOnHover}
-            // onSelect={handleOnSelect}
-            // onFocus={handleOnFocus}
-            autoFocus
-            formatResult={formatResult}
-          />
-          <InputRightElement h="-webkit-fill-available">
-            <SearchIcon
+      <Flex w="60vw" direction="column" position="relative" zIndex="999">
+        <Flex>
+          <InputGroup mt="10">
+            <Input
+              onChange={(event) => setValue(event.target.value)}
+              h={{ base: '2em', lg: '2.5rem' }}
+              bg="gray.700"
+              w="60vw"
+              opacity="0.9"
+              borderRadius="17px"
               color="white"
-              opacity={loopOpacity}
-              mr={{ base: '2', lg: '4' }}
+              fontWeight="700"
+              filter="drop-shadow(9px 7px 20px rgba(0, 0, 0, 0.4))"
+              border="none"
+              fontSize={{ base: 'revert', lg: 'md' }}
+              placeholder="Поиск..."
+              _placeholder={{
+                color: 'white',
+                opacity: 0.3,
+                fontFamily: 'Inter',
+                fontWeight: '700',
+              }}
+              onInput={() => setLoopOpacity('1')}
             />
-          </InputRightElement>
-        </InputGroup>
+            <InputRightElement h="-webkit-fill-available">
+              <SearchIcon
+                color="white"
+                opacity={loopOpacity}
+                mr={{ base: '2', lg: '4' }}
+              />
+            </InputRightElement>
+          </InputGroup>
+        </Flex>
+        <ul className="autocomplete">
+          {value
+            ? fitlerCabinets.map((item, index) => (
+                <li className="autocompleteItem" key={item.id}>
+                  {item.name}
+                </li>
+              ))
+            : null}
+        </ul>
       </Flex>
     </Center>
   )
