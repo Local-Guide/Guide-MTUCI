602704e276a50f2383f12d6046b0752735e78383
Ningaro <23503775+Ningaro@users.noreply.github.com>
2022-08-16 19:24:58 +0300

    💄 Remove unused Logo props

--- src/screens/Logo.tsx
+++ src/screens/Logo.tsx
@@ -2,15 +2,5 @@ import { Image } from '@chakra-ui/react'
 import logo from '../assets/imgs/logo.svg'
 
 export default function Logo() {
-  return (
-    <Image
-      src={logo}
-      opacity="0.6"
-      position="absolute"
-      zIndex="9999"
-      w={{ base: '3em', lg: '7em' }}
-      ml={{ base: '1.8em', lg: '9em' }}
-      mt={{ base: '3em', lg: '0.8em' }}
-    />
-  )
+  return <Image src={logo} opacity="0.6" w={{ base: '4em', lg: '6em' }} />
 }
